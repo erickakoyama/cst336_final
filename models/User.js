2143ee058821fd49b9d58f0029e66c2d85947f9e
erickakoyama <ericka.koyama@gmail.com>
2020-12-06 03:30:12 +0000

    reuse connection for all requests

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
-const createDBConnection = require('./db.js');
+const pool = require('../dbPool.js');
 
 const getByUsername = (username) => {
   const sql = `SELECT * FROM customers WHERE email = ?`;
-  const conn = createDBConnection();
 
   return new Promise((resolve, reject) => {
-    conn.query(sql, [username], (err, rows, fields) => {
+    pool.query(sql, [username], (err, rows, fields) => {
       if (err) { throw err; }
       resolve(rows);
     });

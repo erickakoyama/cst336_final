7d02bf2c289690122d9859fce179d3d1bc485e4d
justinthomascsumb <justhomas@csumb.edu>
2020-12-11 06:46:07 +0000

    moved page loads into Ericka functions, used join instead of 1 query, updated js functions for fetch

--- app.js
+++ app.js
@@ -35,12 +35,25 @@ app.get('/login', ...commonUIMiddlewares, (req, res) => {
 });
 
 app.get('/customer/:id', [...commonUIMiddlewares, middlewares.auth], (req, res) => {
-  res.render('customerProfile');
+  let custQuery = "select * from customers join pets on customers.customer_id where customers.customer_id = ?";
+  let custParams = [req.params.id];
+  pool.query(custQuery, custParams, function(err, rows, fields){
+    if(err) throw err;
+    console.log(err);
+    res.render('customerProfile', {"rows":rows});
+  });
 });
 
-app.get('/service/new', [...commonUIMiddlewares, middlewares.auth], (req, res) => {
-  res.render('scheduleService');
-});
+app.get('/service/new:id', [...commonUIMiddlewares, middlewares.auth], (req, res) => {
+   // get pets
+  let petQuery = "select * from pets, services where pets.customer_id = ?";
+  let petParams = [req.params.id];
+  pool.query(petQuery, petParams, async function(err, rows, fields){
+    if(err) throw(err);
+    console.log(rows);
+    res.render("scheduleService", {"rows":rows});
+  });
+});// /services/new:id
 
 app.post('/login', async(req, res) => {
   const username = req.body.username;
@@ -66,31 +79,10 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-// renders profile page with customer info and pets
-app.get("/customerProfile", function (req, res){
-  // get customer
-  let custQuery = "SELECT * FROM customers WHERE customer_id = ?";
-  let custParams = [req.customerId];
-  let customer = pool.query(custQuery, custParams, function(err, rows, fields){
-    if(err) throw err;
-    console.log(err);
-  });
-  
-  // get pets
-  let petQuery = "SELECT * FROM pets WHERE customer_id = ?";
-  let pets = pool.query(petQuery, custParams, function(err, rows, fields){
-    if(err) throw err;
-    console.log(err);
-  });
-  
-  req.render("customerProfile", {"customer": customer, "pets": pets});
-});// customerProfile
-
 // updates and returns customer (no pet info)
-app.put("api/updateProfile", function (req, res){
-  
+app.put("/api/updateProfile", function (req, res){
   let custQuery = "UPDATE customers WHERE customer_id = SET first_name = ?, last_name = ?, email = ?, phone = ?, address_1 = ?, address_2 = ?, city = ?, sate = ?, zip = ?";
-  let custParam = [req.customerId, req.query.fn, req.query.ln, req.query.email, req.query.phone, req.query.addr1, req.query.addr2, req.query.city, req.query.state, req.query.zip];
+  let custParam = [req.query.customerId, req.query.fn, req.query.ln, req.query.email, req.query.phone, req.query.addr1, req.query.addr2, req.query.city, req.query.state, req.query.zip];
   
   // returns updated customer
   pool.query(custQuery, custParam, function (err, rows, fields){
@@ -101,29 +93,8 @@ app.put("api/updateProfile", function (req, res){
   
 });// api/updateProfile
 
-// renders service page with pets and services
-app.get("/scheduleService", function(req, res){
-  // get pets
-  let petQuery = "SELECT * FROM pets WHERE customer_id = ?";
-  let petParams = [req.customerId];
-  let pets = pool.query(petQuery, petParams, function(err, rows, fields){
-    if(err) throw(err);
-    console.log(rows);
-  });
-  
-  // get services
-  let serviceQuery = "SELECT * FROM services";
-  let services = pool.query(serviceQuery, function(err, rows, fields){
-    if(err) throw(err);
-    console.log(rows);
-  });
-  
-  res.render("scheduleService", {"pets":pets, "services":services});
-  
-});// scheduleService
-
 // schedules selected pet for selected service
-app.post("api/schedulePet", function(req, res){
+app.post("/api/scheduleService", function(req, res){
   let sqlQuery = "INSERT INTO schedule VALUES (?,?,?)";
   let sqlParams = [req.query.date, req.query.serviceId, req.query.petId];
   
@@ -132,22 +103,22 @@ app.post("api/schedulePet", function(req, res){
     console.log(rows);
     res.send(true); // returns true if success, show success image / text
   });
-});// api/schedulPet
+});// api/scheduleService
 
 // checks in a pet based on id
-app.put("api/checkin", function(req, res){
+app.put("/api/checkin/:action/:petId", function(req, res){
   let sqlQuery;
   let sqlParams;
-  let status = req.query.action?1:0;
-  switch(req.query.action){
-    case true:
-              sqlQuery = "UPDATE pets SET checked_in = ? WHERE pet_id = ?";
-              sqlParams = [status, req.query.petId];
-              break;
-    case false:
-              sqlQuery = "UPDATE pets SET checked_in = ? WHERE pet_id = ?";
-              sqlParams = [status, req.query.petId];
-              break;
+  let status = req.params.action?1:0;
+  switch(status){
+    case 0:
+            sqlQuery = "UPDATE pets SET checked_in = ? WHERE pet_id = ?";
+            sqlParams = [status, req.params.petId];
+            break;
+    case 1:
+            sqlQuery = "UPDATE pets SET checked_in = ? WHERE pet_id = ?";
+            sqlParams = [status, req.params.petId];
+            break;
   }
   
   pool.query(sqlQuery, sqlParams, function(err, rows, fields){

d2ca14a952fe96b9a6b387d2ef13005ac5c8ae6f
erickakoyama <ericka.koyama@gmail.com>
2020-12-12 02:51:28 +0000

    Reformat customerProfile endpoint

--- models/User.js
+++ models/User.js
@@ -1,16 +1,41 @@
 const pool = require('../dbPool.js');
 
+const resolveCallback = (resolve) => (err, rows, fields) => {
+  if (err) { throw err; }
+  console
+  resolve(rows);
+}
+
 const getByUsername = (username) => {
   const sql = `SELECT * FROM customers WHERE email = ?`;
 
   return new Promise((resolve, reject) => {
-    pool.query(sql, [username], (err, rows, fields) => {
+    pool.query(sql, [username], resolveCallback(resolve));
+  });
+}
+
+const getCustomerProfileById = async(customerId) => {
+  const customerQuery = `SELECT * FROM customers WHERE customer_id = ?`;
+  const customerPetsQuery = `SELECT * FROM pets WHERE customer_id = ?`;
+
+  const [customer] = await new Promise((resolve, reject) => {
+    pool.query(customerQuery, [customerId], (err, rows) => {
       if (err) { throw err; }
       resolve(rows);
     });
   });
+
+  const customerPets = await new Promise((resolve, reject) => {
+    pool.query(customerPetsQuery, [customerId], resolveCallback(resolve));
+  });
+
+  return {
+    ...customer,
+    pets: customerPets
+  }
 }
 
 module.exports = {
-  getByUsername
+  getByUsername,
+  getCustomerProfileById
 }
